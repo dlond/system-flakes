9bfec6ce89a8cf0fab71c122ae24b34585525599
dlond <dlond@me.com>
2025-05-15 10:27:53 +1200

    update

--- modules/home/base.nix
+++ modules/home/base.nix
@@ -1,5 +1,4 @@
 { lib, pkgs, inputs, ... }:
 {
-    # imports = (import ../../lib/import-modules.nix { inherit lib; }) { dir = ./programs; };
     imports = (lib.importModules { dir = ./programs; inherit lib pkgs inputs; });
 }

0c1be734a604e00cdb0373140acedae49a65e788
dlond <dlond@me.com>
2025-05-15 00:13:21 +1200

    minimal

--- flake.nix
+++ flake.nix
@@ -1,75 +1,51 @@
 {
-  description = "My multi-system Nix configuration flake";
+  description = "Minimal working darwin flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-
-    # Home Manager
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-darwin.url = "github:lnl7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    # Darwin Inputs
-    nix-darwin.url = "github:LnL7/nix-darwin/master";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
-    mac-app-util.url = "github:hraban/mac-app-util";
-
-    # neovim config
-    nvim-config = {
-      url = "github:dlond/nvim";
-      flake = false;
-    };
-
-    # Linux Inputs
-    # nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = inputs@{ self, nixpkgs, nix-darwin, home-manager, nvim-config, ... }:
+  outputs = { self, nixpkgs, nix-darwin, home-manager, ... }:
     let
-      lib = nixpkgs.lib // {
-        importModules = import ./lib/import-modules.nix { lib = nixpkgs.lib; };
-        hm = inputs.home-manager.lib.hm;
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
       };
     in {
-      # Overlays can be defined in overlays/ directory
-      # overlays.default = import ./overlays;
+      darwinConfigurations.mbp = nix-darwin.lib.darwinSystem {
+        inherit system;
 
-      # NixOS configurations e.g.
-      # nixosConfigurations.rpi = nixpkgs.lib.nixosSystem {
-      #   system = "aarch64-linux";
-      #   specialArgs = { inherit inputs; } # Pass down inputs
-      #   modules = [ ./hosts/rpi/default.nix ];
-      # };
+        modules = [
+          # This defines the user account — needed for `home-manager.users.dlond`
+          {
+            users.users.dlond = {
+              name = "dlond";
+              home = "/Users/dlond";
+              shell = pkgs.zsh;
+            };
+          }
 
-      # Darwin configurations
-      darwinConfigurations."mbp" = nix-darwin.lib.darwinSystem {
-          system = "aarch64-darwin";
+          # Home Manager module
+          home-manager.darwinModules.home-manager
 
-          modules = [
-            ./hosts/mbp/default.nix
-            inputs.home-manager.darwinModules.home-manager
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = false;
-                backupFileExtension = "bak";
-                users.dlond = ./home/users/dlond;
-              };
-            }
-          ];
+          # Home Manager config
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = false;
 
-          specialArgs = {
-            inherit inputs lib;
-          };
+            home-manager.users.dlond = import ./home/users/dlond;
+          }
+        ];
+
+        specialArgs = {
+          inherit pkgs;
         };
       };
-
-      # Home Manager configurations (if managing separately)
-      # homeConfigurations."your-username@mbp" = home-manager.lib.homeManagerConfiguration {
-      #   pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-      #   extraSpecialArgs = { inherit inputs; };
-      #   modules = [ ./home/users/dlond/default.nix ];
-      # };
+    };
 }
 
--- hosts/mbp/default.nix
+++ hosts/mbp/default.nix
@@ -1,33 +1,10 @@
-{ inputs, pkgs, lib, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-  # Import reusable modules
-  imports = [
-    ../../modules/common/global.nix
-    ../../modules/darwin/base.nix
-    ../../modules/darwin/fonts.nix
-    ../../modules/darwin/homebrew.nix
-
-    # Add the external modules here:
-    inputs.nix-homebrew.darwinModules.nix-homebrew
-    # inputs.mac-app-util.darwinModules.default
-
-    # Add other custom modules as needed, e.g.:
-    # ../../modules/darwin/gui-apps.nix # If you separate GUI apps
-  ];
-
-  # Specific settings for this host 'mbp'
-  networking.hostName = "mbp";
-
-  # The platform the configuration will be used on.
-  nixpkgs.hostPlatform = "aarch64-darwin";
-
-  # Set Git commit hash for darwin-version
-  # system.configurationRevision comes from the base module now.
-
-  # Any other 'mbp' specific overrides or configurations go here
-  environment.etc."direnv/direnv.toml".text = ''
-    [global]
-    hide_env_diff = true
-  '';
+  users.users.dlond = {
+    name = "dlond";
+    home = "/Users/dlond";
+    shell = pkgs.zsh;
+  };
 }
+
